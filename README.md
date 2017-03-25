47fb4d0fdd8ccdbb0eb304dab62b0354c66b040c
JFlahive32 <jflahive@bbns.org>
2017-03-24 22:02:42 -0400

    1.0.0 Commit
    Data access through SMART on FHIR Sandbox. Tool accesses both personal
    patient data and ALL patient data. Tool comparison in progress and not
    working, so this is not committed. UI is unpolished and unfinished.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# PrecisionCancerMedicine
+Precision Cancer Medicine Tool
+=======
+
+## Goals
+
+Shift off of SMART Sandbox (thus to DSTU3).
+Improve comparison effectiveness.
+
+## Synopsis
+
+SMART on FHIR application. Compare genotypes of similar patients within a FHIR server. Currently works solely with the SMART Sandbox (https://sandbox.smarthealthit.org).
+
+## Installation
+
+`Node.js` / `NPM` is required. 
+
+To build & test web-tool:
+
+```
+git clone https://github.com/Makiah/PrecisionCancerMedicine
+cd PrecisionCancerMedicine
+npm install
+
+# launch web-tool
+npm start
+```
+
+Now, create an account on https://sandbox.smarthealthit.org and register a new app.
+
+```
+App Type: Public Client
+App Name: SMART PCM Tool
+App Launch URI: 
+APP Redirect URIs: 
+Allow Offline Access: No
+Patient Scoped App: Yes
+```
+
+Clicking launch on this new registered application will bring up a list of patients. The chosen patient will be compared to the rest of the patients within the server.
---
+++ server.js
@@ -0,0 +1,24 @@
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var flash 		   = require('flash')
+//==================================================================
+
+// get all data/stuff of the body (POST) parameters
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(flash());
+
+// routes ==================================================
+//require('./app/routes')(app); // pass our application into our routes
+
+// start app ===============================================
+var server = app.listen(process.env.PORT || 8080, "0.0.0.0", function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+});
+
+exports = module.exports = app; // expose app
